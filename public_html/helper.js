33067714628f2a521f786f582da5d0b161c6ccbc
qjohnson <csquinlan@gmail.com>
2014-10-02 14:23:19 +0000

    Experimenting with large image

--- public_html/helper.js
+++ public_html/helper.js
@@ -196,7 +196,7 @@ helper.displayPersonWindow = function(person) {
                     xtype: 'panel', 
                     width: 200,
                     height: 400,
-                    html: '<img src="' + person.picture.medium + '"style="max-height:100%; max-width:100%;"/>',
+                    html: '<img src="' + person.picture.large + '"style="max-height:100%; max-width:100%;"/>',
                     border: false
                 },
                 {
